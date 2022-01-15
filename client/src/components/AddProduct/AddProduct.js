be791b60f4e66e01c5833cc39ba73d89a29b6923
Rajesh Roshan <rajesh.roshan23205@gmail.com>
2022-01-15 08:46:30 +0530

    multer added

--- client/src/components/AddProduct/AddProduct.js
+++ client/src/components/AddProduct/AddProduct.js
@@ -1,13 +1,71 @@
 import React from "react";
+import "./AddProduct.css";
 
-class AddProduct extends React.Component{
-    render(){
-        return(
-            <div>Add Product
-                <button className="btn btn-success">Primary</button>
-            </div>
-        )
-    }
+class AddProduct extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      image: "",
+    };
+  }
+
+  handleChange(event) {
+    console.log("abc");
+    let stateName = event.target.getAttribute("data-stateName");
+    let dataValue = event.target.value;
+    this.setState({ stateName: dataValue });
+  }
+
+  render() {
+    return (
+      <div>
+        Add Product1
+        <form>
+          <div className="mb-3">
+            <label htmlFor="inputEmail" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              aria-describedby="emailHelp"
+              value={this.state.email}
+              data-stateName="email"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="inputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword1"
+              value={this.state.password}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="formFile" className="form-label">
+              Default file input example
+            </label>
+            <input
+              className="form-control"
+              type="file"
+              id="formFile"
+              value={this.state.password}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddProduct;
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,37 +1,54 @@
 var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const multer = require("multer");
 const Products = require("../schema/productModal");
 
-router.get('/', (req, res) => {
-    res.send('Hello World!!!')
-})
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+router.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
 
 const movieSchema = {
-    name:String
-  }
-  
-const Movies = mongoose.model('movies', movieSchema);
+  name: String,
+};
 
-router.get('/movies', function(req, res) {
-  Movies.find().then(movies => res.json(movies))
-})
+const Movies = mongoose.model("movies", movieSchema);
 
-router.post("/setProduct", function(req, res, next){
+router.get("/movies", function (req, res) {
+  Movies.find().then((movies) => res.json(movies));
+});
+
+router.post(
+  "/setProduct",
+  upload.single("productImage"),
+  function (req, res, next) {
     var reqData = req.body;
+    console.log("reqData", req.file, req.body);
     var productSchema = new Products(reqData);
-    productSchema.save(function(error){
-      if(error){
-        console.log("error "+error);
-        res.json({res:"error"});
-      }else{
-        res.json({res:"success"});
+    productSchema.save(function (error) {
+      if (error) {
+        console.log("error " + error);
+        res.json({ res: "error" });
+      } else {
+        res.json({ res: "success" });
       }
-    })
-  })
+    });
+  }
+);
 
-  router.get('/getProduct', function(req, res) {
-    Products.find().then(movies => res.json(movies))
-  })
+router.get("/getProduct", function (req, res) {
+  Products.find().then((movies) => res.json(movies));
+});
 
-  module.exports = router;
+module.exports = router;
